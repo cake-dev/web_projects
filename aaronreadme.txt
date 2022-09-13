739fba57de119ceaba9ea3922878ff2f73270d60
Jake <developer.montana@gmail.com>
2022-09-13 11:57:23 -0600

    deleted:    Jake.txt 	deleted:    aaronreadme.txt 	modified:   pages/__pycache__/urls.cpython-310.pyc 	modified:   pages/__pycache__/views.cpython-310.pyc 	modified:   pages/urls.py 	modified:   pages/views.py 	new file:   templates/about.html 	new file:   templates/base.html 	modified:   templates/home.html

--- aaronreadme.txt
+++
@@ -1,3 +0,0 @@
-Hi my name is Aaron. I'm studying CS at the U of M along with MIS and cybersecurity.
-I'm hoping to do software engineering or cybersecurity consulting.
-I have a doggo named Marco.
--- pages/views.py
+++ pages/views.py
@@ -7,5 +7,6 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
